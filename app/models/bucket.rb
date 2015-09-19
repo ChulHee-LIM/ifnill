28e1af5894bff47baf9c52e150a2a85b6c3d60b7
Hwang Junwoo <capollux10@gmail.com>
2015-09-19 23:19:55 +0900

    빈칸 예외 처리

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -10,4 +10,23 @@ class Bucket < ActiveRecord::Base
 	has_many :supports
 	has_many :replies
 
+	validates :name, :presence => true
+	validates :intro_simple, :presence => true
+	validates :intro_detail, :presence => true
+	validates :finish_date, :presence => true, format: { with: /\A\d+-\d+-\d+\z/}
+	validates :start_date, :presence => true, format: { with: /\A\d+-\d+-\d+\z/}
+
+
+	def self.change_bucket_state
+		timeNow = Time.now.in_time_zone('Seoul').to_date
+		Bucket.all.each do |b|
+		time_diff_components = Time.diff(b.finish_date, timeNow, '%d')[:day]
+		  if time_diff_components == 0
+		    if b.state == 0
+		      b.update(:state => 1)
+		    end 
+		  end
+		end
+	end
+
 end
